c13af324ffa673d3fa6b51698492c2776d762fb1
Piotr Przybysz <piotr.przybysz.dev@gmail.com>
2026-01-28 20:59:58 +0100

    Update default scene

--- Path-Tracing/Resources.h
+++ Path-Tracing/Resources.h
@@ -7,5 +7,8 @@ namespace PathTracing::Resources
 {
 
 extern const std::array<const uint8_t, 213869> g_PlaceholderTextureData;
+extern const std::array<const uint8_t, 10670> g_VulkanPathTracingTextureData;
+extern const std::array<const uint8_t, 12618> g_AuthorsTextureData;
+extern const std::array<const uint8_t, 47229> g_PressSpaceTextureData;
 
 }
